8379fcbc1ed8ce79264492fb0fe45acfb81bf30c
Harsh Agrawal <97937899+harshu1611@users.noreply.github.com>
2024-05-21 17:58:16 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,9 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/blog/:resource/:id/show': '/:resource/:id',
+    '/products/:id'
+
 }))
 server.use(router)
 server.listen(3000, () => {
